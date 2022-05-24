e7b3545ac7ae1d476d083fbced30383b5d22d42e
Artem <temaqwerty15@gmail.com>
2022-05-24 13:15:01 +0300

    study props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card, { CardVariant } from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Card
+                width='200px'
+                height='200px'
+                variant={CardVariant.outline}
+                onClick={(num) => console.log(num,'test clik')}
+            >
+                <button>Click</button>
+            </Card>
+        </div>
+    );
 }
 
 export default App;
